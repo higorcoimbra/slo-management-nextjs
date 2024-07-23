bd1205ab420f30e89338d43a27d47d6e751237ba
Higor Coimbra <higor1308@gmail.com>
2024-07-23 20:54:47 -0300

    feat: hello world

---
+++ slo-management/src/pages/slos/__tests__/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { Slos } from '..';
+
+describe('SLO Page', () => {
+    it('should render the Slos page', () => {
+        render(<Slos />);
+    });
+})
